f4d55327938202bcc1fbe003cd9d4db4bf046e37
son-kun-1 <d.dimambro7@gmail.com>
2026-03-31 13:33:58 -0400

    clarify testing logic

--- hardware/tests/mcp3008_test.py
+++ hardware/tests/mcp3008_test.py
@@ -5,8 +5,11 @@ spi.open(0, 0)  # bus 0, CE0
 spi.max_speed_hz = 1_000_000
 
 def read_channel(ch):
+    # format: [1, channel (8-15), 0]
     r = spi.xfer2([1, (8 + ch) << 4, 0])
+    # keep bottom 2 bits of r[1], left shift + OR with r[2] to get full 10-bit value
     return ((r[1] & 3) << 8) | r[2]
+    
 while True:
     for ch in range(2):
         raw = read_channel(ch)
